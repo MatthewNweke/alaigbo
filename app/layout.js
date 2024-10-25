a9a106b50b640ee56d6c4c1223fa422062504f04
Matthew Nweke <nwekematthew243@gmail.com>
2024-10-25 15:32:21 +0100

    comlete

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,10 @@
 import Header from '@/components/Header';
 import './globals.css';
-import { Inter } from 'next/font/google';
 import Footer from '@/components/Footer';
 import Provider from '@/components/Provider';
 import { Toaster } from '@/components/ui/toaster';
 import '@uploadthing/react/styles.css';
 
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: ' Harnessing Human Capital for Sustainable Development in Alaigbo',
@@ -16,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Provider>
           <Header />
 
